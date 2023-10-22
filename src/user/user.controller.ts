87429392edd0bd6635e4ef2aeffb54154dee1c44
Andrey Gorelik <bublic007@inbox.ru>
2023-10-23 01:01:29 +0300

    user crud with validation pipe

---
+++ src/user/user.controller.ts
@@ -0,0 +1,47 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
+import { UserService } from './user.service';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+
+@Controller('user')
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+
+  @Post()
+  create(@Body() createUserDto: CreateUserDto) {
+    // try {
+    //   return this.userService.createUser(createUserDto);
+    // } catch (error) {
+    //   throw new BadRequestException(error.message);
+    // }
+    return this.userService.createUser(createUserDto);
+  }
+
+  @Get()
+  findAll() {
+    return this.userService.findAllUser();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.userService.viewUser(+id);
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
+    return this.userService.updateUser(+id, updateUserDto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.userService.removeUser(+id);
+  }
+}
